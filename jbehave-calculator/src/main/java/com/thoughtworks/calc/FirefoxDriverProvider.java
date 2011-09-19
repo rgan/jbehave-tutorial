b2f06d3aa6e7ad4de7952ea7f60fb3518fadc9e7
rgan <rajaram.ganeshan@gmail.com>
2011-09-19 16:28:19 -0400

    Comment on how to start firefox driver with a profile directory

--- jbehave-calculator/src/main/java/com/thoughtworks/calc/FirefoxDriverProvider.java
+++ jbehave-calculator/src/main/java/com/thoughtworks/calc/FirefoxDriverProvider.java
@@ -3,6 +3,9 @@ package com.thoughtworks.calc;
 import org.jbehave.web.selenium.WebDriverProvider;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+
+import java.io.File;
 
 public class FirefoxDriverProvider implements WebDriverProvider {
     private WebDriver driver;
@@ -12,6 +15,10 @@ public class FirefoxDriverProvider implements WebDriverProvider {
     }
 
     public void initialize() {
+        // Sometimes you want to start firefox with a specific profile.
+        // On windows, profiles are typically found in: c:\Documents and Settings\<user>\Application Data\Firefox\Profiles
+        //FirefoxProfile profileDir = new FirefoxProfile(new File("C:\\some-dir"));
+        //driver = new FirefoxDriver(profileDir);
         driver = new FirefoxDriver();
     }
 
